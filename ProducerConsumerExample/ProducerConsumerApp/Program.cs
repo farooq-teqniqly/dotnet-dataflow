1426a9e89a9268d587a38e5c075cb7c60434c934
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-05-21 17:25:27 -0700

    producer/consumer

--- ProducerConsumerExample/ProducerConsumerApp/Program.cs
+++ ProducerConsumerExample/ProducerConsumerApp/Program.cs
@@ -7,31 +7,32 @@ internal class Program
     static async Task Main(string[] args)
     {
         var urlsToProcess = new[] { "https://www.espn.com", "https://www.microsoft.com", "https://www.google.com" };
-        var processQueue = new BufferBlock<string>(new DataflowBlockOptions { BoundedCapacity = urlsToProcess.Length });
-        var random = new Random();
-
-        foreach (var url in urlsToProcess)
-        {
-            await Task.Delay(TimeSpan.FromSeconds(random.Next(5, 10)));
-            Console.WriteLine($"Queueing {url}...");
-            await processQueue.SendAsync(url);
-        }
 
         var consumerOptions = new ExecutionDataflowBlockOptions { BoundedCapacity = 1 };
-
         var consumerA = new ActionBlock<string>(url => Console.WriteLine($"Processing {url}..."), consumerOptions);
         var consumerB = new ActionBlock<string>(url => Console.WriteLine($"Processing {url}..."), consumerOptions);
         var consumerC = new ActionBlock<string>(url => Console.WriteLine($"Processing {url}..."), consumerOptions);
 
         var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
-
+        var processQueue = new BufferBlock<string>(new DataflowBlockOptions { BoundedCapacity = urlsToProcess.Length });
         processQueue.LinkTo(consumerA, linkOptions);
         processQueue.LinkTo(consumerB, linkOptions);
         processQueue.LinkTo(consumerC, linkOptions);
 
-        await Task.WhenAll([processQueue.Completion, consumerA.Completion, consumerB.Completion, consumerC.Completion]);
+        var random = new Random();
+
+        foreach (var url in urlsToProcess)
+        {
+            Console.WriteLine($"Queueing {url}...");
+            await Task.Delay(TimeSpan.FromSeconds(random.Next(2, 5)));
+            processQueue.SendAsync(url);
+        }
+
         processQueue.Complete();
+        await Task.WhenAll([processQueue.Completion, consumerA.Completion, consumerB.Completion, consumerC.Completion]);
+
 
         Console.WriteLine("Done!");
+        Console.ReadKey();
     }
 }
